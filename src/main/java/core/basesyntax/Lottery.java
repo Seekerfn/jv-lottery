3057bd9c4b5f78cf807ed4eb1ce9ecdc01b3d6f3
Zarytskyi Oleksandr <pfantom180@gmail.com>
2026-05-11 20:48:26 +0300

    changed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,18 +4,14 @@ import java.util.Random;
 
 public class Lottery {
 
-    
     private static final int MAX_BALL_NUMBER = 100;
 
-    
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
 
-        
         ball.setNumber(random.nextInt(MAX_BALL_NUMBER + 1));
 
         ball.setColor(colorSupplier.getRandomColor());
